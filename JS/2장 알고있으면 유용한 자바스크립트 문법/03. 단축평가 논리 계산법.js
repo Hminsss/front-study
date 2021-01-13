bb343f099fe170c60251969ee346109453506e9e
hanminsssss <gksals6464@kyonggi.ac.kr>
2021-01-13 18:12:18 +0900

    Update 03. 단축평가 논리 계산법.js

--- JS/2장 알고있으면 유용한 자바스크립트 문법/03. 단축평가 논리 계산법.js
+++ JS/2장 알고있으면 유용한 자바스크립트 문법/03. 단축평가 논리 계산법.js
@@ -6,8 +6,44 @@ const dog = {
 };
 
 function getName(animal) {
-  if (animal) {
-    return animal.name;
-  }
-  return undefined;
+  return animal && animal.name;
 }
+
+const name = getName();
+console.log(name);
+
+// 앞쪽이 true이면 뒤쪽이 출력됨
+console.log(true && 'hello'); // truthy 출력
+console.log(false && 'hello'); // falsy false 출력
+console.log('hello' && 'bye'); // truthy bye 출력
+console.log(null && 'hello'); // falsy null출력
+console.log(undefined && 'hello'); // falsy undefined 출력
+
+// or 연산자
+
+const namelessDog = {
+  name: '',
+};
+
+function getNames(animal) {
+  const name = animal && animal.name;
+  if (!name) {
+    return '이름이 없는 동물입니다.';
+  }
+  return name;
+}
+
+const name2 = getNames(namelessDog);
+console.log(name2);
+
+function getNamess(animal) {
+  const name = animal && animal.name;
+  return name || '이름이 없는 동물입니다.';
+}
+
+// 앞쪽이 false 이면 출력
+console.log(true || 'hello'); // truthy true 출력
+console.log(false || 'hello'); // falsy hello 출력
+console.log('hello' || 'bye'); // truthy hello 출력
+console.log(null || 'hello'); // falsy hello출력
+console.log(undefined || 'hello'); // falsy hello 출력
